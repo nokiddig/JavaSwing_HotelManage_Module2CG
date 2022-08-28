86ef8a63c5ab15aa879f842dc4fbe681a1e301a9
nokiddig <sy2000dn0@gmail.com>
2022-08-28 22:31:37 +0700

    add reply JTArea

--- src/view/adminManager/JPInformation.java
+++ src/view/adminManager/JPInformation.java
@@ -7,10 +7,12 @@ import java.awt.*;
 
 public class JPInformation extends JPanel {
     private final Font font = new Font("Serif", Font.ITALIC|Font.BOLD, 20);
+    private final Font jlFont = new Font("times new roman", Font.ITALIC, 16);
     private JLabel jlNoti = new JLabel("Notification: ");
-    private JLabel jlInfo = new JLabel("Information: ");
+    private JLabel jlRep = new JLabel("Reply: ");
     private JTextArea jtaNoti = new JTextArea(10, 10);
-    private JTextArea jtaInfo = new JTextArea(10, 10);
+    private JTextArea jtaRep = new JTextArea(10, 10);
+    private JButton jbSend, jbQuit, jb;
 
     public JPInformation () {
         this.setupBorder("Hello");
@@ -22,22 +24,41 @@ public class JPInformation extends JPanel {
     private void addComponent() {
         this.add(jlNoti);
         this.add(jtaNoti);
-        this.add(jtaInfo);
-        this.add(jlInfo);
+        this.add(jtaRep);
+        this.add(jlRep);
     }
 
     private void setupComponent() {
         this.setupJLNoti();
+        this.setupJTANoti();
+        this.setupJLRep();
+        this.setupJTARep();
     }
 
     private void setupJLNoti() {
         jlNoti = new JLabel("Notification: ");
-        jlNoti.setFont(new Font("times new roman", Font.ITALIC, 16));
+        jlNoti.setFont(jlFont);
         jlNoti.setForeground(Color.BLACK);
         jlNoti.setSize(150, 30);
         jlNoti.setLocation(20, 20);
     }
+    private void setupJTANoti() {
+        jtaNoti.setSize(275, 250);
+        jtaNoti.setLocation(10, 50);
+        jtaNoti.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
 
+    private void setupJLRep() {
+        jlRep.setFont(jlFont);
+        jlRep.setForeground(Color.black);
+        jlRep.setSize(150, 30);
+        jlRep.setLocation(20, 300);
+    }
+    private void setupJTARep() {
+        jtaRep.setSize(275, 100);
+        jtaRep.setLocation(10, 330);
+        jtaRep.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
     private void setupBorder(String name) {
         Border border = BorderFactory.createLineBorder(Color.black, 1);
         this.setBorder(BorderFactory.createTitledBorder(border, name,
